f2e4b7922c341eda11243698d2cd45300343e9d9
Sarah Campos <sarahcampos997@gmail.com>
2024-03-23 19:53:39 -0300

    Project created

---
+++ src/main/java/com/educandoweb/educandoWeb/EducandoWebApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.educandoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducandoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducandoWebApplication.class, args);
+	}
+
+}
